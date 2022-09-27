0c30c523f690bb9f3d523d3edd4d816d3d55a511
xuhg <xuhg@dpark.com.cn>
2022-09-27 17:20:34 +0800

    ....

---
+++ createGz.sh
@@ -0,0 +1,7 @@
+tar zcvf ./v1-baseUC.tar.gz ./v1-basedUC
+tar zcvf ./v1-mapmost.tar.gz ./v1-mapmost
+tar zcvf ./v1-mobile.tar.gz ./v1-mobile
+tar zcvf ./v1-sipsdCloudUI.tar.gz ./v1-sipsdCloudUI
+tar zcvf ./v1-static.tar.gz ./v1-static
+tar zcvf ./v2-1.tar.gz ./v2-1
+tar zcvf ./v2-2.tar.gz ./v2-2
