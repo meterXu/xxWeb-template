beaf070bd2ed483d688b84c9e8a5c485d1cd7027
xuhg <xuhg@dpark.com.cn>
2022-10-09 09:32:24 +0800

    ....

--- v1-basedUC/src/views/Login.vue
+++ v1-basedUC/src/views/Login.vue
@@ -5,10 +5,10 @@
 <script>
 import {util} from '@dpark/s2-utils'
 export default {
-    name:'login',
-    mounted(){
-      util.redirectSsoLogin(this.$project_{{ namespace }},this)
-    }
+  name:'login',
+  mounted(){
+    util.redirectSsoLogin(this.$project_{{ namespace }},this)
+  }
 }
 </script>
 
