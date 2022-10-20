576f6d23bc17b588f66c1424bbf59bab1e06e220
xuhg <xuhg@dpark.com.cn>
2022-10-20 16:39:00 +0800

    ....

--- v2-1/src/api/dealwithError.js
+++ v2-1/src/api/dealwithError.js
@@ -2,26 +2,31 @@ import {request as requestCreate,portal} from '@dpark/s2-xxweb-utils';
 import Vue from 'vue';
 
 export function dealWithError(error){
-  let data = error.response.data;
-  if(typeof(data)==='string'&&data.indexOf('{')>-1){
-    data = JSON.parse(data);
-  }
-  data.message = (data.msg||data.message)||requestCreate.getErrorText(error.response.status)
-  switch (error.response.status){
-  case 500:
-    if (data.message === 'Token失效，请重新登录') {
-      showConfirm()
-    }else{
-      Vue.prototype.$notify.error({title: '系统提示', message: data.message, duration: 4});
+  if(error.response){
+    let data = error.response.data;
+    if(typeof(data)==='string'&&data.indexOf('{')>-1){
+      data = JSON.parse(data);
     }
-    break
-  case 401:{
-    showConfirm()
-    break
-  }
-  default:
-    Vue.prototype.$notify.error({title: '系统提示', message: data.message, duration: 4});
-    break
+    data.message = (data.msg||data.message)||requestCreate.getErrorText(error.response.status)
+    switch (error.response.status){
+    case 500:
+      if (data.message === 'Token失效，请重新登录') {
+        showConfirm()
+      }else{
+        Vue.prototype.$notify.error({title: '系统提示', message: data.message});
+      }
+      break
+    case 401:{
+      showConfirm()
+      break
+    }
+    default:
+      Vue.prototype.$notify.error({title: '系统提示', message: data.message});
+      break
+    }
+    throw Error(data)
+  }else{
+    throw Error(error.message)
   }
 }
 
--- v2-2/src/api/dealwithError.js
+++ v2-2/src/api/dealwithError.js
@@ -1,27 +1,32 @@
-import {request as requestCreate} from '@dpark/s2-xxweb-utils';
+import {request as requestCreate,portal} from '@dpark/s2-xxweb-utils';
 import Vue from 'vue';
 
 export function dealWithError(error){
-  let data = error.response.data;
-  if(typeof(data)==='string'&&data.indexOf('{')>-1){
-    data = JSON.parse(data);
-  }
-  data.message = (data.msg||data.message)||requestCreate.getErrorText(error.response.status)
-  switch (error.response.status){
-  case 500:
-    if (data.message === 'Token失效，请重新登录') {
-      showConfirm()
-    }else{
-      Vue.prototype.$notify.error({title: '系统提示', message: data.message, duration: 4});
+  if(error.response){
+    let data = error.response.data;
+    if(typeof(data)==='string'&&data.indexOf('{')>-1){
+      data = JSON.parse(data);
     }
-    break
-  case 401:{
-    showConfirm()
-    break
-  }
-  default:
-    Vue.prototype.$notify.error({title: '系统提示', message: data.message, duration: 4});
-    break
+    data.message = (data.msg||data.message)||requestCreate.getErrorText(error.response.status)
+    switch (error.response.status){
+    case 500:
+      if (data.message === 'Token失效，请重新登录') {
+        showConfirm()
+      }else{
+        Vue.prototype.$notify.error({title: '系统提示', message: data.message});
+      }
+      break
+    case 401:{
+      showConfirm()
+      break
+    }
+    default:
+      Vue.prototype.$notify.error({title: '系统提示', message: data.message});
+      break
+    }
+    throw Error(data)
+  }else{
+    throw Error(error.message)
   }
 }
 
@@ -32,7 +37,7 @@ function showConfirm (){
     confirmButtonText:'重新登录',
     callback:function (action){
       if(action==='confirm'){
-        util.logOut(Vue)
+        portal.logout(window.project)
       }
     }
   })
