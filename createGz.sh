4be2bd0267d3276ed01c0cd479c5ecf6feebfd1b
xuhg <xuhg@dpark.com.cn>
2022-12-07 10:09:22 +0800

    ....

--- createGz.sh
+++ createGz.sh
@@ -4,3 +4,5 @@ tar zcvf ./v1-mobile.tar.gz ./v1-mobile
 tar zcvf ./v1-static.tar.gz ./v1-static
 tar zcvf ./v2-1.tar.gz ./v2-1
 tar zcvf ./v2-2.tar.gz ./v2-2
+tar zcvf ./resp-onemap-template.tar.gz ./resp-onemap-template
+tar zcvf ./resp-onemap-template-dist.tar.gz ./resp-onemap-template-dist
